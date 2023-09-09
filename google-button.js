0ac976acdfb81ed8cf7b2c2b93a2f027127f436b
Luis <eternal_pitta0b@icloud.com>
2023-09-09 12:05:32 -0500

    update google calendar scripts

--- google-button.js
+++ google-button.js
@@ -1,11 +1,11 @@
 (function() {
-    var target = document.currentScript;
-    window.addEventListener('load', function() {
-      calendar.schedulingButton.load({
-        url: 'https://calendar.google.com/calendar/appointments/schedules/AcZssZ3HS1Khx_LaueRVzDaEcXNxMau2cQqntCNxEfI9gqxpna3K7WboG8Hshi8mEgTyrjil4ra-PkN1?gv=true',
-        color: '#fe6600',
-        label: "Schedule an Estimate",
-        target,
-      });
+  var target = document.currentScript;
+  window.addEventListener('load', function() {
+    calendar.schedulingButton.load({
+      url: 'https://calendar.google.com/calendar/appointments/schedules/AcZssZ3kgOkZT76W15KQb-RB0EI3FPq3LG-SrYltV_H3cF2sHQS8w-Kz5xGHFJxLci2c6KNy1enecels?gv=true',
+      color: '#fe6600',
+      label: "Schedule an Estimate",
+      target,
     });
-  })();
+  });
+})();
